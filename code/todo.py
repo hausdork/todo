63b43ffedaa29fc846d7335ab6e7bd2f9d335e93
hausdork <euanscott19@gmail.com>
2017-10-27 15:21:02 +0100

    Add files via upload
    nothing of value added, last commit of day

--- code/todo.py
+++ code/todo.py
@@ -1,18 +1,38 @@
 import os
-
-#from flask import Flask
-#app = Flask(__name__)
+from flask import request
+from flask import Flask
+app = Flask(__name__)
 
 dir = os.path.dirname(__file__)
 filename = os.path.join(dir, 'todo.md')
 
-todolist = open(filename)
 items = []
+incomplete = []
+complete = []
 
+todolist = open(filename, "r")
 for line in todolist:
     items.append(line)
+todolist.close()
 
-print(items[2:])
-#@app.route('/',methods=['GET'])
-#def hello_world():
-#    return todolist
+for i in items:
+    if i[:3] == "[ ]":
+        incomplete.append(i)
+    elif i[:3] == "[x]":
+        complete.append(i)
+
+# RESTful API
+
+@app.route('/tasks',methods=['GET'])
+def gettasks():
+    tmp = ""
+    for i in incomplete:
+        tmp = tmp + i
+    tmp = tmp + "\n"
+    for i in complete:
+        tmp = tmp + i
+    return tmp
+
+        
+        
+    
